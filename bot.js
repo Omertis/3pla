a999db5147cb8356c039749f145319c7078e7f07
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-22 00:22:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const http2 = require('http2');
 
-const prefix = "1"
+const prefix = "3"
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NDcwMTExNTY3NDUxNzgzMTc4.DjRhkg.aT1zl6Df3zd6ArWy-GCINatkIec";
+const discord_token = "NDcwMTg0ODU3MDIxMDU0OTg2.DjSnNA.elZNLCIg7lnK60ynWiuPaeOpwEk";
 client.login(discord_token);
 client.on('ready', function () {
     console.log(`Logged in as * [ " ${client.user.username} " ]`);
@@ -474,7 +474,7 @@ client.on('message', message => {
     });
 
 client.on('ready', () => {
-  client.user.setGame('[1help]..[1play]','https://www.twitch.tv/pd13');
+  client.user.setGame('[3help]..[3play]','https://www.twitch.tv/pd13');
 });
 
-client.login("NDcwMTExNTY3NDUxNzgzMTc4.DjRhkg.aT1zl6Df3zd6ArWy-GCINatkIec");
+client.login("NDcwMTg0ODU3MDIxMDU0OTg2.DjSnNA.elZNLCIg7lnK60ynWiuPaeOpwEk");
