ef1f3520994a250761193298df2a5e21d8fb16c0
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-28 14:17:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.TOKEN = 'NDcxMjM5OTIwMzUwOTIwNzA0.Dj3Xhw.zbv_T6w9VHQ8Oyhp6KW9CV6Va48'; // ya wr3 la tns5
+exports.TOKEN = 'NDcxMjM5OTIwMzUwOTIwNzA0.Dj3hVQ.M5IKL-Lmf9nxFzPIhYt1yVsPAFI'; // ya wr3 la tns5
 
 exports.PREFIX = '3';
 
